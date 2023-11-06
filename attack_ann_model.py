aa08eafbfcbba19a0a72d92275b8d59c4b777cb6
Jasnoski <f646b388@headnode02.beoshock.wichita.edu>
2023-11-05 23:01:13 -0600

    Add code to create bar chart

--- attack_ann_model.py
+++ attack_ann_model.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 import sklearn.metrics as metrics
 
+import matplotlib.pyplot as plt
+
 # Load the model
 model_stin = keras.models.load_model('saved-rf-sfs-ann.keras')
 
@@ -30,3 +32,29 @@ accuracy_affected_data = metrics.accuracy_score(y, np.argmax(predictions, axis=1
 
 print(f"Accuracy using unmodified data {accuracy_clean_data:.2f}%")
 print(f"Accuracy using modified data {accuracy_affected_data:.2f}%")
+
+# Data for the bars
+categories = ['Unmodified Data', 'Modified Data']
+values = [accuracy_clean_data, accuracy_affected_data]  # These values should represent percentages
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Set the y-axis limit to 100% and the x-axis limit to 0%
+ax.set_ylim(0, 100)
+
+# Create the bar chart
+bars = ax.bar(categories, values, color=['green', 'red'])
+
+# Add labels to the bars
+for bar in bars:
+    height = bar.get_height()
+    ax.annotate(f'{height}%', (bar.get_x() + bar.get_width() / 2, height), ha='center', va='bottom')
+
+# Add labels and title
+ax.set_xlabel('Categories')
+ax.set_ylabel('Percentage')
+ax.set_title('Attack Accuracy')
+
+# Show the chart
+plt.savefig('acc.png')
